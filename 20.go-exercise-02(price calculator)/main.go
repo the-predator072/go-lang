da2dc5317d15b2da90782f46f79f6cd50fa8f7db
Vikas Negi <72794288+the-predator072@users.noreply.github.com>
2024-12-21 21:13:16 +0530

    added log

--- 20.go-exercise-02(price calculator)/main.go
+++ 20.go-exercise-02(price calculator)/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	taxRates := []float64{0, 0.7, 0.1, 0.15}
+	fmt.Println("hi there")
 	for _, taxRate := range taxRates {
 		fm := filemanager.New("prices.txt", fmt.Sprintf("results_%v.json", taxRate*100))
 		priceJob := prices.New(fm, taxRate)
